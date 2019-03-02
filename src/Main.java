ed3528f2221e1a8e77c986aef48ddce0e70f9faf
johannathiemich <johanna.thiemich@gmx.de>
2019-03-01 16:12:22 -0800

    Added some classes, adapted .gitignore

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import javazoom.jlgui.basicplayer.BasicPlayer;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MusicPlayerGUI player = new MusicPlayerGUI();
+        player.setVisible(true);
+    }
+}
---
+++ src/PlayButtonListener.java
@@ -0,0 +1,9 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PlayButtonListener implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
