565c920243de8c8f8c6babf651b07a4d3b78c0bb
Jellabie <devjellabie@gmail.com>
2019-03-10 05:20:15 -0700

    Add listeners to buttons and a slider at MainController

--- src/main/java/MainController.java
+++ src/main/java/MainController.java
@@ -1,5 +1,12 @@
 import javazoom.jlgui.basicplayer.BasicPlayer;
 
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.plaf.basic.BasicOptionPaneUI;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
  * This controller is a Supervising Controller or messenger
  * between Model(data,entity) and View(interface,gui)
@@ -22,12 +29,58 @@ public class MainController {
      * Construct a main controller and initialize all modules
      */
     public MainController(){
-        playerView = new MusicPlayerGUI("MyTunes 0.0");
+        playerView = new MusicPlayerGUI("MainController Testing");
         library = new SongLibrary();
         db = new DatabaseHandler();
         player = new BasicPlayer();
+
+        playerView.setVisible(true);
+
+        addListeners();
+
     }
 
+    //TODO add inner classes of ActionListener for Table, Buttons, Slider
+    public void addListeners(){
+        //Buttons
+        playerView.startSong.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("PLAY button is pressed.");
+            }
+        });
+        playerView.stopSong.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("STOP button is pressed.");
+            }
+        });
+        playerView.prevSong.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("PREVIOUS button is pressed.");
+            }
+        });
+        playerView.nextSong.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("NEXT button is pressed.");
+            }
+        });
 
-    //TODO add inter classes of ActionListener for Table, Buttons, Slider
+        //Slider
+        playerView.scrollVolume.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                JSlider source = (JSlider)e.getSource();
+                System.out.println("Slider tick: " + source.getValue());
+
+                if (!source.getValueIsAdjusting()) {
+                    int volume = source.getValue();
+                    System.out.println("Volume: " + volume);
+                }
+
+            }
+        });
+    }
 }
