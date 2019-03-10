5b6c147d253cb3bec5f1b9aa9587b1f2cb3003a1
Jellabie <devjellabie@gmail.com>
2019-03-09 17:11:51 -0800

    add a new SongLibrary class

---
+++ src/main/java/SongLibrary.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+
+public class SongLibrary {
+    private ArrayList<Song> songList;
+    //what else?
+
+    /**
+     * Construct an empty library
+     */
+    public SongLibrary(){
+        //initialize songArrayList...?
+
+    }
+
+    /**
+     * Construct a library from an array of songs
+     * @param songArray
+     */
+    public SongLibrary(ArrayList<Song> songArray){
+        songList = songArray;
+    }
+
+    /**
+     * Add a song to the library, if not present.
+     * @param song song to be added to the list
+     */
+    public void addSong(Song song){
+        if(songList.contains(song)){
+            System.out.println("The song '"+song.getTitle()+"' already exist in the library");
+        }else{
+            songList.add(song);
+        }
+    }
+
+    /**
+     * Add multiple songs to the library, if not present.
+     * @param songArray songs to be added to the list
+     */
+    public void addSong(ArrayList<Song> songArray){
+        for (Song song : songArray) {
+            addSong(song);
+        }
+    }
+
+    /**
+     * Delete a song from the library, if present.
+     * @param song song to be removed from the list
+     */
+    public void deleteSong(Song song){
+        if(songList.contains(song)) {
+            songList.remove(song);
+        }else{
+            System.out.println("The song '"+song.getTitle()+"' does not exist in the library");
+        }
+    }
+
+}
