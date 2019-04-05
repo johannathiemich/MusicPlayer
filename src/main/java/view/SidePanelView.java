d42987346a2eb71543ecc8a3227c954dbcfe849e
sellabae <devsellabae@gmail.com>
2019-04-04 23:35:12 -0700

    Remove the icons from tree view in the side panel

--- src/main/java/view/SidePanelView.java
+++ src/main/java/view/SidePanelView.java
@@ -3,6 +3,7 @@ package view;
 
 import javax.swing.*;
 import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeCellRenderer;
 import javax.swing.tree.DefaultTreeModel;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
@@ -12,43 +13,65 @@ import java.awt.event.MouseAdapter;
  * and a tree of the existing playlists.
  */
 public class SidePanelView extends JPanel {
-    //components for the side panel
-    private JScrollPane scrollPane;
+    private Dimension sidePanelSize = new Dimension(120,500);
+
+    //components for library tree
+    private JPanel libraryTreePanel;
     private JTree libraryTree;
-    private JTree playlistTree;
     private DefaultTreeModel libTreeModel;
-    private DefaultTreeModel pllTreeModel;
     private DefaultMutableTreeNode libRootNode;
+    //components for playlist tree
+    private JScrollPane playlistTreePanel;
+    private JTree playlistTree;
+    private DefaultTreeModel pllTreeModel;
     private DefaultMutableTreeNode pllRootNode;
 
-    private Dimension sidePanelSize = new Dimension(120,500);
+    private DefaultTreeCellRenderer renderer;
 
     /**
      * Constructs a panel to show "Library" and "Playlist" with a tree.
      */
     public SidePanelView() {
-        //creates a Library tree with one node
+        //creates a "Library" tree with only one node (the root node)
         libRootNode = new DefaultMutableTreeNode("Library");
         libTreeModel = new DefaultTreeModel(libRootNode);
         libraryTree = new JTree(libTreeModel);
+        libraryTreePanel = new JPanel();
+        libraryTreePanel.add(libraryTree);
 
-        //create a Playlist tree with playlists
+        //create a "Playlist" tree with playlist nodes
         pllRootNode = new DefaultMutableTreeNode("Playlist");
+        pllTreeModel = new DefaultTreeModel(pllRootNode);
+        playlistTree = new JTree(pllTreeModel);
+        playlistTreePanel = new JScrollPane(playlistTree);
+
+        //call the list of playlist names from the db
+        //create nodes under "Playlist"
         DefaultMutableTreeNode pll1Node = new DefaultMutableTreeNode("Playlist 1");
         DefaultMutableTreeNode pll2Node = new DefaultMutableTreeNode("Playlist 2");
         DefaultMutableTreeNode pll3Node = new DefaultMutableTreeNode("Playlist 3");
         pllRootNode.add(pll1Node);
         pllRootNode.add(pll2Node);
         pllRootNode.add(pll3Node);
-        pllTreeModel = new DefaultTreeModel(pllRootNode);
-        playlistTree = new JTree(pllTreeModel);
+        //reload tree model to display updated tree nodes
+        pllTreeModel.reload();
 
-        //call the list of playlist names from the db
-        //create nodes under "Playlist"
 
-        scrollPane = new JScrollPane(playlistTree);
+        //tree cell renderer setup for trees
+        renderer = new DefaultTreeCellRenderer();
+        //remove default icons
+        renderer.setLeafIcon(null);
+        renderer.setClosedIcon(null);
+        renderer.setOpenIcon(null);
+        //apply cell renderer to trees
+        libraryTree.setCellRenderer(renderer);
+        playlistTree.setCellRenderer(renderer);
+
+
+        //put tree panels in place
         this.setLayout(new BorderLayout());
-        this.add(scrollPane, BorderLayout.CENTER);
+        this.add(libraryTreePanel, BorderLayout.NORTH);
+        this.add(playlistTreePanel, BorderLayout.CENTER);
 
         this.setPreferredSize(sidePanelSize);
     }
@@ -73,17 +96,24 @@ public class SidePanelView extends JPanel {
         //..
     }
 
-    public void setColorTheme(ColorTheme colorTheme) {
-        //background color setup
+    public void setColorTheme(final ColorTheme colorTheme) {
+        //this panel color setup
         this.setBackground(colorTheme.bgColor[1]);
-        scrollPane.setOpaque(false);
-        playlistTree.setBackground(colorTheme.bgColor[1]);
-        scrollPane.setBorder(BorderFactory.createEmptyBorder());
-        //playlistTree.setBorder(BorderFactory.createEmptyBorder());
         this.setBorder(BorderFactory.createLineBorder(colorTheme.bgColor[0]));
-        //text color setup
-        libraryTree.setForeground(colorTheme.fgColor[0]);
-        playlistTree.setForeground(colorTheme.fgColor[0]);
+        //tree panel color setup
+        libraryTreePanel.setOpaque(false);
+        libraryTree.setBackground(colorTheme.bgColor[1]);
+        libraryTreePanel.setBorder(BorderFactory.createEmptyBorder());
+        playlistTreePanel.setOpaque(false);
+        playlistTree.setBackground(colorTheme.bgColor[1]);
+        playlistTreePanel.setBorder(BorderFactory.createEmptyBorder());
 
+        //tree cell color setup
+        renderer.setBackgroundNonSelectionColor(null);
+        renderer.setTextNonSelectionColor(colorTheme.fgColor[0]);
+        renderer.setBackgroundSelectionColor(colorTheme.pointColor[0]);
+        renderer.setTextSelectionColor(colorTheme.pointColor[1]);
+        renderer.setBackground(null);
     }
+
 }
